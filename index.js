d975d78a18f3e4cdcf85bb6c91d0c5a578d3acf9
Alex Betancourt <alex@Alexs-MacBook-Pro.local>
2021-12-25 21:58:35 -0500

    set up constructors in lib/, input prompts after initializing and set to push info into empty array for each employee, created add another member function

--- index.js
+++ index.js
@@ -1,36 +1,77 @@
+// const inquirer = require("inquirer");
 const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Employee = require("./lib/Employee");
 const Intern = require("./lib/Intern");
-const 
 const teamMemberArr = [];
+const addMember = () => {
+    console.log(`
+    =======================
+    Add New Member to Team
+    =======================
+    `);
+    return inquirer.prompt ({
+        type: 'confirm',
+        name: 'confirmAddMember',
+        message: 'Would you like to add a new employee to this team?',
+        default: false
+    })
+}
 
-const teamMembers = {
+const teamMembers = () => {
+    inquirer.prompt([
+        {
   type: "list",
   name: "title",
   message: "Choose a team member to add?",
   choices: ["Manager", "Engineer", "Employee", "Intern", "Team Complete - Generate Team"],
-}.then((answer) => {
+}])
+.then((answer) => {
   if (answer === "Manager") {
-    let newMember = 
+    let newManager = new Manager();
+    let addNewManager = newManager.createNewManager();
+    teamMembersArr.push(addNewManager);
   } else if (answer === "Engineer") {
-    let newMember = 
+    let newEngineer = new Engineer();
+    let addNewEngineer = newEngineer.createNewEngineer();
+    teamMembersArr.push(addNewEngineer);
   } else if (answer === "Employee") {
-    let newMember = 
+    let newEmployee = new Employee();
+    let addNewEmployee = newEmployee.createNewEmployee();
+    teamMembersArr.push(addNewEmployee);
   } else if (answer === "Intern") {
-    let newMember = 
-  }
+    let newIntern = new Intern();
+    let addNewIntern = newIntern.createNewIntern();
+    teamMembersArr.push(addNewIntern);
 
+  } else if (answer === "Team Complete - Generate Team") {
+      return
+  }
+})
+
+.then (() => {
+    addMember()
+})
+.then ((answer) => {
+    console.log (answer);
+    if (answer === true) {
+        teamMembers();
+    } else {
+        return teamMemberArr
+    }
 });
+};
 
 // TODO: Create a function to initialize app
 function init() {
-  inquirer.prompt(teamMembers)
-    //   .then((data) => {
-    //   console.log(questions);
-    //   console.log(data);
-    . then (writeToFile("teamMembers.html", generatePage(teamMemberArr)))};
+    
+  teamMembers();
+//     //   .then((data) => {
+//     //   console.log(questions);
+//     //   console.log(data);
+//     . then (writeToFile("teamMembers.html", generatePage(teamMemberArr)))
+};
 
 
 // Function call to initialize app
